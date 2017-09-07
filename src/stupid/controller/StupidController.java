3a8d40678aef1be18ad400b6effd3f71fb1c167a
greenRose2018 <brifun13@gmail.com>
2017-09-07 08:56:08 -0600

    added code to Controller

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -5,6 +5,7 @@ public class StupidController
 	public void start()
 	{
 		System.out.println("My name is Brittney, I like the color green.");
+		System.out.println("I love to read books.");
 	}
 
 }
