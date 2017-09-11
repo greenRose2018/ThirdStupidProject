fb96e547ada498a3ffc3e5df35a9acef03101998
greenRose2018 <brifun13@gmail.com>
2017-09-11 07:30:46 -0600

    How to use a Scanner, (to talk to the computer.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,11 +1,24 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
 public class StupidController
 {
 	public void start()
 	{
 		System.out.println("My name is Brittney, I like the color green.");
 		System.out.println("I love to read books.");
+		
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("Please type your favorite food");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh, you like to eat " + answer);
 	}
 
 }
